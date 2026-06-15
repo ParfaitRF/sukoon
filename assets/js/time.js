4bd6aa27f31f5e4a5e02046f7aa7f75b23d27545
divyanshchandhok <divyanshchandhok@users.noreply.github.com>
2026-06-15 09:14:49 +0530

    Enhance accent color visibility, color weekday, and style weather elements with accent

--- assets/js/time.js
+++ assets/js/time.js
@@ -28,7 +28,7 @@ function displayClock() {
 	document.getElementById('separator').innerHTML = ' : ';
 	document.getElementById('minutes').innerText = min + ampm;
 
-	document.getElementById('fullDate').innerText = `${dayOfWeek}, ${mm} ${dd}`;
+	document.getElementById('fullDate').innerHTML = `<span class="dateAccent">${dayOfWeek}</span>, ${mm} ${dd}`;
 
 	setTimeout(displayClock, 1000);
 }
